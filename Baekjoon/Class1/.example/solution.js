98ffe03dc529a97a4614035cc2537a1e276d7bad
splin <dev.splin@gmail.com>
2024-10-09 19:17:30 +0900

    백준 : example 입력에 trim 추가

--- Baekjoon/Class1/.example/solution.js
+++ Baekjoon/Class1/.example/solution.js
@@ -1,3 +1,3 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt"
-const input = fs.readFileSync(filePath).toString().split(' ');
+const input = fs.readFileSync(filePath).toString().trim().split(' ');
